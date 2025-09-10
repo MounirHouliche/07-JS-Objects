3f6cd099b2dc8b26b585148635426b67fa0da91d
MounirHouliche <mounir_rojos@outlook.es>
2025-09-10 11:58:16 +0100

    added files

---
+++ app.js
@@ -0,0 +1,23 @@
+// Creating objects & Accessing Properties
+
+const person ={
+    name: "Bob",
+    age: 30,
+    favouriteColour: "red",
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.favouriteColour);
+
+
+// Adding Properties
+
+const persons ={
+    name: "Bob",
+    age: 30,
+    favouriteColour: "red",
+};
+
+persons.favouriteFood = "pizza";
+console.log(persons.favouriteFood)
